6e525cfaa2cc3d84bf3bbc69b41d6ab6a286840d
serafimsng <serafimsinaga@gmail.com>
2020-02-21 11:17:05 +0700

    edited teamController added delete team function, edited team.test

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -100,7 +100,13 @@ class teamController {
         })
     }
     static deleteTeam(req,res,next){
-        
+        Team.findByIdAndDelete({_id: req.params.teamId})
+        .then(data=>{
+            res.status(200).json(data)
+        })
+        .catch(err=>{
+            res.status(400).json(err)
+        })
     }
 }
 
