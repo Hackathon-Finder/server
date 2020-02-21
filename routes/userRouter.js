371734760ef781176b55fbdc35d0b2bf94ce26fd
kennysalam <kennys.salam@gmail.com>
2020-02-21 18:30:53 +0700

    add google login, fix hp to not required

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,12 +3,14 @@ const express = require('express')
 const router = express.Router()
 const User = require('../controllers/userController')
 const authentication = require('../middlewares/authentication')
+const googleAuth = require('../middlewares/googleAuth')
 
 router.get('/', User.findAllUser)
 router.get('/:userId', User.findOne)
 router.post('/register', User.register)
 router.post('/skill', User.findBySkill)
 router.post('/login', User.login)
+router.post('/glogin/:id_token', googleAuth, User.glogin)
 router.patch('/', authentication, User.update)
 
 module.exports = router
