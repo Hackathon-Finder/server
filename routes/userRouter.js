d23081dbde9c2517a9469b603b2fc4541140d48c
kennysalam <kennys.salam@gmail.com>
2020-02-24 10:50:53 +0700

    add user update pict upload middleware

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,7 @@ const User = require('../controllers/userController')
 const authentication = require('../middlewares/authentication')
 const googleAuth = require('../middlewares/googleAuth')
 const {reviewAuthorization} = require('../middlewares/authorization')
+const upload = require('../middlewares/aws-upload')
 
 router.get('/', User.findAllUser)
 router.get('/:userId', User.findOne)
@@ -12,7 +13,7 @@ router.post('/register', User.register)
 router.post('/skill', User.findBySkill)
 router.post('/login', User.login)
 router.post('/glogin/:id_token', googleAuth, User.glogin)
-router.patch('/', authentication, User.update)
+router.patch('/', authentication, upload.single('pict'), User.update)
 router.patch('/review/:userId', authentication, reviewAuthorization, User.updateReview)
 
 module.exports = router
